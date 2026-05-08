ac4fd7eaf86ce8e722ef9e62eb314dcbbc77ef99
Kadir Güneş <88841126+KadirGunes@users.noreply.github.com>
2026-05-08 17:36:48 +0300

    Add files via upload

---
+++ SQL_FORM/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace SQL_FORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.ConnectText = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.SelectTable = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(633, 11);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(102, 58);
+            this.Connect.TabIndex = 0;
+            this.Connect.Text = "Bağlan";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // ConnectText
+            // 
+            this.ConnectText.Location = new System.Drawing.Point(468, 47);
+            this.ConnectText.Name = "ConnectText";
+            this.ConnectText.Size = new System.Drawing.Size(159, 22);
+            this.ConnectText.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 148);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 293);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // SelectTable
+            // 
+            this.SelectTable.FormattingEnabled = true;
+            this.SelectTable.Location = new System.Drawing.Point(282, 45);
+            this.SelectTable.Name = "SelectTable";
+            this.SelectTable.Size = new System.Drawing.Size(151, 24);
+            this.SelectTable.TabIndex = 5;
+            this.SelectTable.SelectedIndexChanged += new System.EventHandler(this.SelectTable_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 453);
+            this.Controls.Add(this.SelectTable);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ConnectText);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "Veritabanı Gözlemcicsi";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.TextBox ConnectText;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox SelectTable;
+    }
+}
+
---
+++ SQL_FORM/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SQL_FORM
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        private bool isConnected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SelectTable.Visible = false;
+        }
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            connection = 
+                new SqlConnection("server=(local)\\SQLExpress;database=" + ConnectText.Text + ";integrated Security=SSPI;");
+
+            try
+            {
+                connection.Open();
+                isConnected = true;
+                SelectTable.Visible = true;
+                MessageBox.Show("Bağlantı gerçekleşti");
+            }
+            catch
+            {
+                MessageBox.Show("Bağlantı kurulamadı.");
+                SelectTable.Visible = false;
+                dataGridView1.DataSource = null;
+                return;
+            }
+            
+            DataTable tableNames = connection.GetSchema("Tables", new string[] {null, null, null, "BASE TABLE"});
+
+            DataTableReader reader = tableNames.CreateDataReader();
+
+            while(reader.Read())
+            {
+                string tableName = reader["TABLE_NAME"].ToString();
+                if (tableName == "sysdiagrams") continue;
+
+                SelectTable.Items.Add(tableName);
+            }
+        }
+
+        private void SelectTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter adapter;
+
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "select * from " + SelectTable.Text;
+
+            adapter = new SqlDataAdapter(command.CommandText, connection);
+
+            DataSet data = new DataSet();
+            adapter.Fill(data, SelectTable.Text);
+            dataGridView1.DataSource = data.Tables[SelectTable.Text];
+        
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
+        }
+    }
+}
